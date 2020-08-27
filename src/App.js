ff324b9d735303116ab63f7493884fd43d0f65d3
Szuhanszky <tamas@Szuhanszkys-MacBook-Pro.local>
2020-08-27 08:51:21 +0200

    UI5 Tree

--- src/App.js
+++ src/App.js
@@ -2,26 +2,39 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import "@ui5/webcomponents/dist/Button";
+import "@ui5/webcomponents/dist/Tree.js";
+import "@ui5/webcomponents/dist/TreeItem.js";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <p>This is a UI5 Element</p>
-        <ui5-button>Hello world!</ui5-button>
-      </header>
+      <p>This is a UI5 Tree</p>
+      <ui5-tree id="myTree" class="full-width">
+        <ui5-tree-item expanded text="Tree 1" icon="paste" selected>
+          <ui5-tree-item expanded text="Tree 1.1" selected>
+            <ui5-tree-item text="Tree 1.1.1"></ui5-tree-item>
+            <ui5-tree-item text="Tree 1.1.2"></ui5-tree-item>
+          </ui5-tree-item>
+        </ui5-tree-item>
+        <ui5-tree-item text="Tree 2" icon="copy">
+          <ui5-tree-item text="Tree 2.1">
+            <ui5-tree-item text="Tree 2.1.1"></ui5-tree-item>
+            <ui5-tree-item text="Tree 2.1.2">
+              <ui5-tree-item text="Tree 2.1.2.1"></ui5-tree-item>
+              <ui5-tree-item text="Tree 2.1.2.2"></ui5-tree-item>
+              <ui5-tree-item text="Tree 2.1.2.3"></ui5-tree-item>
+              <ui5-tree-item text="Tree 2.1.2.5"></ui5-tree-item>
+            </ui5-tree-item>
+          </ui5-tree-item>
+          <ui5-tree-item text="Tree 2.2"></ui5-tree-item>
+        </ui5-tree-item>
+
+        <ui5-tree-item expanded text="Tree 3 (no icon)">
+        </ui5-tree-item>
+      </ui5-tree>
+      
+      <p>This is a UI5 Button</p>
+      <ui5-button>Hello world!</ui5-button>
     </div>
   );
 }
