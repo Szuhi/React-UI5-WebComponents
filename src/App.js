ffa6408ea2685f0a546d0585c9cd7b47bbc234b2
Szuhanszky <tamas@Szuhanszkys-MacBook-Pro.local>
2020-08-27 11:02:04 +0200

    Remove list

--- src/App.js
+++ src/App.js
@@ -4,19 +4,10 @@ import './App.css';
 import "@ui5/webcomponents/dist/Button";
 import "@ui5/webcomponents/dist/Tree.js";
 import "@ui5/webcomponents/dist/TreeItem.js";
-import "@ui5/webcomponents/dist/List.js";
-import "@ui5/webcomponents/dist/StandardListItem.js";
 
 function App() {
   return (
     <div className="App">
-      <p>This is a UI5 List</p>
-      <ui5-list id="myList" class="full-width">
-        <ui5-li icon="nutrition-activity" description="Tropical plant with an edible fruit" info="In-stock" info-state="Success">Pineapple</ui5-li>
-        <ui5-li icon="nutrition-activity" description="Occurs between red and yellow" info="Expires" info-state="Warning">Orange</ui5-li>
-        <ui5-li icon="nutrition-activity" description="The yellow lengthy fruit" info="Re-stock" info-state="Information">Blueberry</ui5-li>
-        <ui5-li icon="nutrition-activity" description="The tropical stone fruit" info="Re-stock" info-state="Error">Mango</ui5-li>
-      </ui5-list>
 
       <p>This is a UI5 Tree</p>
       <ui5-tree id="myTree" class="full-width">
