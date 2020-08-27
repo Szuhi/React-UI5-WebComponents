32bec6811b1a089c63a5e42e0adcaa9531b97db3
Szuhanszky <tamas@Szuhanszkys-MacBook-Pro.local>
2020-08-27 10:38:51 +0200

    UI5 List

--- src/App.js
+++ src/App.js
@@ -4,10 +4,20 @@ import './App.css';
 import "@ui5/webcomponents/dist/Button";
 import "@ui5/webcomponents/dist/Tree.js";
 import "@ui5/webcomponents/dist/TreeItem.js";
+import "@ui5/webcomponents/dist/List.js";
+import "@ui5/webcomponents/dist/StandardListItem.js";
 
 function App() {
   return (
     <div className="App">
+      <p>This is a UI5 List</p>
+      <ui5-list id="myList" class="full-width">
+        <ui5-li icon="nutrition-activity" description="Tropical plant with an edible fruit" info="In-stock" info-state="Success">Pineapple</ui5-li>
+        <ui5-li icon="nutrition-activity" description="Occurs between red and yellow" info="Expires" info-state="Warning">Orange</ui5-li>
+        <ui5-li icon="nutrition-activity" description="The yellow lengthy fruit" info="Re-stock" info-state="Information">Blueberry</ui5-li>
+        <ui5-li icon="nutrition-activity" description="The tropical stone fruit" info="Re-stock" info-state="Error">Mango</ui5-li>
+      </ui5-list>
+
       <p>This is a UI5 Tree</p>
       <ui5-tree id="myTree" class="full-width">
         <ui5-tree-item expanded text="Tree 1" icon="paste" selected>
@@ -32,7 +42,7 @@ function App() {
         <ui5-tree-item expanded text="Tree 3 (no icon)">
         </ui5-tree-item>
       </ui5-tree>
-      
+
       <p>This is a UI5 Button</p>
       <ui5-button>Hello world!</ui5-button>
     </div>
