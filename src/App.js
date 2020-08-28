e6d0a6532e362fc5a82846c7616e02e9426ecc37
Cheetah <szuhanszky.tamas@gmail.com>
2020-08-28 09:32:05 +0200

    React.createRef() does not work, but toast can be triggered with another button

--- src/App.js
+++ src/App.js
@@ -9,6 +9,17 @@ import "@ui5/webcomponents/dist/Title";
 import "@ui5/webcomponents/dist/Toast";
 
 class App extends Component {
+  constructor (props) {
+    super(props);
+    this.switch = React.createRef();
+  }
+
+/*   componentDidMount() {
+    this.switch.addEventListener('change', event => {
+        console.log('switch is toggled');
+    })
+  } */
+
   buttonClick = () => {
     console.log("Button clicked");
     const toast = document.getElementById('wcToastBasic');
@@ -20,12 +31,13 @@ class App extends Component {
   render () {
     return (
       <div className="App">
+        <button onClick={this.buttonClick}>Asd</button>
         <div>
           <ui5-title level="H2">UI5 Web Components, React demo application.</ui5-title>
         </div>
         <div className="Content">
           <ui5-label>This is a UI5 Button</ui5-label><br /><br />
-          <ui5-button click={() => this.buttonClick}>Hello!</ui5-button><br />
+          <ui5-button ref={this.switch} click={() => this.buttonClick}>Hello!</ui5-button><br />
           <br />
           <ui5-label>This is a UI5 Tree</ui5-label><br /><br />
           <ui5-tree id="myTree" class="full-width">
